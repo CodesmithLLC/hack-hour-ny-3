991ff512ccb60ee67f33393570f0427777bb0bed
Margaret Ma <eutopia@gmail.com>
2018-03-14 09:35:19 -0400

    changed input from array to integer

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,10 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-  var numArr = num
+  var numArr = []
+  for (var i = 0; i <= num; i++) {
+    numArr.push(i);
+  }
   for (let i = 0; i < numArr.length; i++) {
     if ((numArr[i] % 3 === 0) && (numArr[i] % 5 === 0)) {
       numArr[i] = "fizzbuzz"
