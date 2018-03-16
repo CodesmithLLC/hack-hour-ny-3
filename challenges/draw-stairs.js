0e4f54ef2b508932ec678aee00a342a4c77c11f4
StephenGrable1 <stephengrable@gmail.com>
2018-03-16 11:18:47 -0400

    add stairs hack hour, unsolved

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -13,9 +13,25 @@
 
 */
 
-function drawStairs(n) {
 
+function drawStairs(n) {
+    //I tried to jump straight into using recursion 
+    //I think I could have solved this with a for loop and counter
+    //but I wanted to see if I could understand solving this recursively 
+    //to get some practice but couldn't quite get there 
+    var start = 1;
+    var stairs = "";
+
+    if(n === 0){
+        return;
+    }
+
+    console.log(stairs)
+
+    return drawStairs(n - 1);
 }
+
+drawStairs(3)
 
 
 module.exports = drawStairs;
