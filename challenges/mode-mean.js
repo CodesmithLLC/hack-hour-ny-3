dbb69356f29743f75286398b53236300b179d325
Margaret Ma <eutopia@gmail.com>
2018-03-17 12:54:12 -0400

    finished mode / mean challenge"

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,36 @@
 
 
 function modemean(array) {
+  function add(a, b) {
+    return a + b
+  }
+  var mean = Math.floor(array.reduce(add, 0)/(array.length))
 
+  var counts = {}
+  for (var i = 0; i < array.length; i ++) {
+    if (counts[array[i]]) {
+ 		  counts[array[i]] += 1;
+ 		} else {
+ 		  counts[array[i]] = 1   
+ 		}
+  }
+  var countsVal = Object.values(counts).sort(function(a, b) {
+    return b - a
+  })
+  
+  var mode = Object.keys(counts).filter(function(key) {
+    return counts[key] === countsVal[0]
+  });
+  
+  mode = mode.sort(function(a, b) {
+    return b - a
+  })
+  
+  if (mode[0] == mean) {
+    return true
+  } else {
+    return false
+  }
 }
 
 module.exports = modemean;
