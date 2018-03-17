d12f91cb4e129eb4088055058554bb29a2afe53c
Alex Egorenkov <aegorenk@gmail.com>
2018-03-17 09:56:51 -0400

    First version of modemean to pass tests

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,23 @@
 
 
 function modemean(array) {
-
+    const mean = array.reduce((acc, cur) => acc + cur)/array.length;
+    const modes = array.reduce((counts, cur) => {
+        if (cur in counts) {
+            counts[cur] += 1;
+        } else {
+            counts[cur] = 1;
+        }
+        return counts;
+    }, {});
+    mode = +Object.entries(modes).reduce((acc, cur) => {
+        if (acc[1] === cur[1]) {
+          if (+acc[0] >= +cur[0]) return acc;
+          return cur;
+        }
+        if (acc[1] >= cur[1]) return acc;
+        return cur})[0];
+    return mean === mode;
 }
 
 module.exports = modemean;
