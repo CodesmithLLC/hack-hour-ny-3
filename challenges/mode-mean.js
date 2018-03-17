3597bc02c1d822993d2bf0f9c5928308c964ea45
Richie Walker <RAW@Richies-MacBook-Pro.local>
2018-03-17 10:00:55 -0400

    Hack Hour 3

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,27 @@
  *
  */
 
+ function modemean(array) {
+  let mean = 0;
+  let mode = 0;
+  array.sort((a,b) => a - b);
 
-function modemean(array) {
+  let added = array.reduce((acc, cur) => {
+    return acc + cur
+  }, 0);
+  mean = Math.floor(added / array.length);
 
+  const arrayObj = array.reduce((obj, cur) => {
+    obj[cur] = obj[cur] ? obj[cur] += 1 : 1
+    return obj
+  }, {});
+
+  for (let e in arrayObj){
+    if(arrayObj[e] >= mode){
+    mode = (Number(e) >= mode) ? Number(e) : mode
+    }
+  }
+  return (mode === mean) ? true : false;
 }
 
 module.exports = modemean;
