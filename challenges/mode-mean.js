c8a0ae942451aeee7d128d8a60c55c5c3c253fb1
StephenGrable1 <stephengrable@gmail.com>
2018-03-17 09:50:36 -0400

    finish mean mode

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,47 @@
 
 
 function modemean(array) {
+    let sum = array.reduce((a,b) => {
+        return a+b;
+    })
+    let mean = Math.floor(sum/array.length);
+
+    let freq = {};
+    for(let i = 0; i < array.length; i++){
+        let stringNum = array[i].toString();
+        if(freq[stringNum]){
+            freq[stringNum] += 1;
+        } else {
+            freq[stringNum] = 1;
+        }
+    }
+
+    let mostFreq;
+    let occurs;
+    for(var key in freq){
+        if(!mostFreq){
+            mostFreq = key;
+            occurs = freq[key]
+        }
+        if(freq[key] > occurs){
+            mostFreq = key;
+            occurs = freq[key];
+        }
+        if(freq[key] === occurs && key > mostFreq){
+            mostFreq = key;
+        }
+    }
+
+    console.log(mean, mostFreq)
+
+    if (mostFreq == mean){
+        return true;
+    } else {
+        return false;
+    }
 
 }
+
+console.log(modemean([1,1,1,1,7,7,7,7,7,7,7,7,7]));
 
 module.exports = modemean;
