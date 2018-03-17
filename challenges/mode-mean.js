d416ce4c0e8ff17021f4637b1032a2ba53e8c49b
Alesi-Andreya Ladas <alesiladas@gmail.com>
2018-03-17 10:07:47 -0400

    HH modemean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -1,17 +1,50 @@
 /*
- * Given an array of numbers, determine if the mode and mean of the array are equivalent
+ * Given an sorted of numbers, determine if the mode and mean of the array are equivalent
  *
  * Caveats:
  * 	- Math.floor the mean
  * 	- If there are multiple modes, use the max of the modes
- *
  * Return true or false
- *
+ * mean = avg
+ * mode = most
  */
 
 
 function modemean(array) {
+  var sorted = array.sort(function(a, b) {
+    return a - b;
+  });
 
+  var sum = sorted.reduce(function(acc, value) {
+    return acc += value;
+  }, 0);
+
+  var mean = Math.floor(sum / sorted.length);
+
+  var occurences = {};
+  sorted.forEach(function(el) {
+    if (occurences[el] === undefined) {
+      occurences[el] = 1;
+    } else {
+      occurences[el] += 1;
+    }
+  });
+
+  var mode;
+  var highestVal = 0;
+
+  for (var ele in occurences) {
+    if (occurences[ele] >= highestVal) {
+      if (occurences[ele] === highestVal && Number(occurences[ele]) > Number(mode)) {
+        mode = ele;
+      }
+
+      highestVal = occurences[ele];
+      mode = ele;
+    }
+  }
+
+  return mode == mean;
 }
 
 module.exports = modemean;
