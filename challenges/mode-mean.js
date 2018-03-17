a86b478c31ecf3400a42d6f1336b6569f1beaffc
Haaris-S <haaris.ny@gmail.com>
2018-03-17 09:40:10 -0400

    hack hour modemean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,40 @@
 
 
 function modemean(array) {
-
+  return getMean(array) ===  getMode(array);
 }
 
+function getMean(array) {
+  let average =  array.reduce((a,b) => {
+    return a + b;
+  },0)/array.length;
+
+  return Math.floor(average);
+}
+
+function getMode(array) {
+  let numCount = {};
+  let maxArr = [];
+  let max = null;
+
+  array.forEach(num => {
+    !numCount[num] ? numCount[num] = 1 : numCount[num] += 1;
+  });
+
+  for (let key in numCount) {
+    if (numCount[key] > max || max === null) {
+      maxArr.push(key);
+      max = numCount[key];
+    } else if (numCount[key] === max) {
+      maxArr.push(key);
+    }
+  }
+
+  return Math.max(...maxArr);
+}
+
+let nums = [1,4,6,7,7,7,8,4,4,10,10,10,9,9,9];
+// console.log(getMean(nums));
+// console.log(getMode(nums));
+console.log(modemean(nums));
 module.exports = modemean;
