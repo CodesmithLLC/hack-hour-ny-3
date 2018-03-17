2e7270ccc1bea010b23f70908581cd84034f54c4
DenaliDeMots <denali.demots@gmail.com>
2018-03-17 09:54:36 -0400

    Submission

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,30 @@
 
 
 function modemean(array) {
-
+    modeCount = {}
+    array.forEach(num => {
+        modeCount[num] ? modeCount[num] += 1 : modeCount[num] = 1
+    })
+    mode = Object.entries(modeCount)
+        .map(numPair => {
+            numPair[0] = Number.parseInt(numPair[0], 10)
+            return numPair
+        }).reduce((maxCount, count) => {
+            if (count[1] > maxCount[1]) {
+                return count
+            } else if (count[1] === maxCount[1]) {
+                return count[0] > maxCount[0] ? count : maxCount
+            } else {
+                return maxCount
+            }
+        })[0]
+    mean = Math.floor(
+        array.reduce((num1, num2) => {
+            return num1 + num2
+        })
+        / array.length
+    )
+    return mean === mode
 }
 
 module.exports = modemean;
