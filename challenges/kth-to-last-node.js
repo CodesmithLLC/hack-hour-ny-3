7d713ddd4a703d38a1c281fa19bd889cb4bd81ac
Richie Walker <RAW@Richies-MacBook-Pro.local>
2018-03-20 09:48:51 -0400

    Hack Hour 4

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -2,7 +2,6 @@
  * Write a function that takes two parameters, an integer and the head of a
  * singly linked list, and returns the VALUE on the kth to last node in the list.
  *
- * const a = new Node('A');
  * const b = new Node('B');
  * const c = new Node('C');
  * const d = new Node('D');
@@ -22,7 +21,28 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+  let length = 0;
+  let goal= 0;
+  let result;
 
+  function count(node){
+    length += 1
+    if(node.next){
+      count(node.next)
+    }else return;
+  }
+  count(head);
+
+  function goalCount(node){
+    goal += 1
+    if(goal !== length - k + 1){
+      goalCount(node.next)
+    }else {result = node}
+  }
+  goalCount(head);
+
+  console.log(length,goal,result.value);
+  return (result.value)
 }
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
