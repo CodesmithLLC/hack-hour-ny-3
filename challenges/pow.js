02c97801a6fe9b0e6417e077aba944994e9a8207
StephenGrable1 <stephengrable@gmail.com>
2018-03-21 09:58:49 -0400

    work for negative bases

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,23 +3,14 @@
  */
 
 function pow(base, power) {
-    base = Math.abs(base);
     if (power === 0){      
         return 1
     } else {
         if (power === 1) return base * 1;
-        return Math.abs(base * pow(base, power-1));
+        return base * pow(base, power-1);
     } 
 }
 
-// console.log(pow(20, 2))
-// console.log(pow(-2, 3))
-// console.log(pow(-12, 2))
-// console.log(12*12)
-// console.log(pow(-100, 2))
-// // console.log(100*100)
-// console.log(pow(230, 0))
-
-// console.log(pow(-2, 0))
+console.log(pow(-2,3))
 
 module.exports = pow;
