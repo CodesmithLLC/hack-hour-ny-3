4c6c9963fd16acf271890370599f2cc3cf9b6720
Haaris-S <haaris.ny@gmail.com>
2018-03-21 09:27:56 -0400

    hack hour pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -4,6 +4,16 @@
 
 function pow(base, power) {
 
+  function absolutePower(base, power) {
+    if (power === 0) return 1;
+    power = power < 0 ? power * -1 : power;
+    return base * absolutePower(base, power - 1);
+  }
+
+  let abs = absolutePower(base, power);
+
+  return power < 0 ? 1/abs : abs;
+
 }
 
 module.exports = pow;
