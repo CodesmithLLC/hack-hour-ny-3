e4c4f266e54cb1a58cba8978a574151488c223b8
Alex Egorenkov <aegorenk@gmail.com>
2018-03-21 09:23:35 -0400

    Complete pow.

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,11 @@
  */
 
 function pow(base, power) {
-
+  function _pow(base, power) {
+    if (power === 0) return 1;
+    return base*pow(base, power - 1);
+  }
+  return power > 0 ? _pow(base, power) : 1 / _pow(base, -power);
 }
 
 module.exports = pow;
