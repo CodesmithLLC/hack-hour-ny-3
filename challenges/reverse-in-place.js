86bcfc52dafe6537e5f69a237c8064dbc61d872e
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2018-03-23 09:59:29 -0400

    push

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -14,7 +14,22 @@
  */
 
 function reverseInPlace(array) {
+    let current = 0;
+    let opposite = array.length-1;
+    let center = array.length / 2;
 
+    while (current < center) {
+        let left = array[current];
+        let right = array[opposite];
+        
+        array[current] = right;
+        array[opposite] = left;
+        
+        current++;
+        opposite--;
+    }
+
+    return array;
 }
 
 module.exports = reverseInPlace;
