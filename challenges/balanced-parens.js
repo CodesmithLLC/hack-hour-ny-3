868261ffa24327e446ade8defaf9f225fc21c443
StephenGrable1 <stephengrable@gmail.com>
2018-03-27 10:06:08 -0400

    unsolved balanced parens, working on helper function to compare indexes of forward and backward parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,60 @@
  */
 
 function balancedParens(input){
+    let forwardParens = ['[', '{', '('];
+    let backwardParens = [']', '}', ')'];
 
+    let splitStr = input.split('');
+    let forwardIndexes = {'square': [], 'bracket': [], 'parens': []};
+    let backwardIndexes = {'square': [], 'bracket': [], 'parens': []};
+
+    for (var i = 0; i < splitStr.length; i++){
+        if (forwardParens.indexOf(splitStr[i]) > -1){
+            switch(splitStr[i]){
+                case '[':
+                    forwardIndexes.square.push(i)
+                case '{':
+                    forwardIndexes.bracket.push(i)
+                case '(':
+                    forwardIndexes.parens.push(i)
+            }
+        }
+
+        if (backwardParens.indexOf(splitStr[i]) > -1){
+            switch(splitStr[i]){
+                case ']':
+                backwardIndexes.square.push(i)
+                case '}':
+                backwardIndexes.bracket.push(i)
+                case ')':
+                backwardIndexes.parens.push(i)
+            }
+        }
+    }
+
+    console.log(areTheyBalanced(forwardIndexes, backwardIndexes))
 }
+
+function areTheyBalanced (forwardIndexes, backwardIndexes) {
+    for(var key in forwardIndexes){
+        for (let i = 0; i < forwardIndexes[key].length; i+=1){
+            console.log("Checking out of order: ", backwardIndexes[key][i], forwardIndexes[key][i])
+            if ()
+            if (forwardIndexes[key][i] > backwardIndexes[key][i]){
+                console.log("These are out of order: ", backwardIndexes[key][i], forwardIndexes[key][i])
+                return false;
+            }
+        }
+    }
+    return true;
+    
+}
+
+
+
+// balancedParens('[](){}'); // true
+// balancedParens('[({})]');   // true
+// balancedParens('[  )(  ]  {  )   }'); // false
+balancedParens('()('); // false
 
 module.exports = balancedParens;
