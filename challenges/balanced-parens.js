532657bc33e5e3ee07bf5ccf6eadbeb689e10c20
Alesi-Andreya Ladas <alesiladas@gmail.com>
2018-03-27 10:04:01 -0400

    balanced parens HH

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -23,9 +23,59 @@
  *
  *
  */
+function isEven(num) {
+    return (num % 2 === 0 && num >= 0);
+}
 
-function balancedParens(input){
+function balancedParens(input) {
+    var bracketChars = '(){}[]'.split('');
+    var string = '';
+    var curly = 0;
+    var paren = 0;
+    var bracket = 0;
 
+    for (var i = 0; i < input.length; i++) {
+        if (bracketChars.includes(input[i])) {
+            string += input[i];
+            if (input[i] === '[' || input[i] === ']') {
+                if (bracket === 0 && input[i] === '[') {
+                    bracket += 1;
+                } else {
+                    bracket -= 1;
+                }
+
+                if ((input[i - 1] === '{' && input[i + 1] === '}') || (input[i - 1] === '(' && input[i + 1] === ')')) {
+                    bracket -= 1;
+                }
+            } else if (input[i] === '(' || input[i] === ')') {
+                if (paren === 0 && input[i] === '(') {
+                    paren += 1;
+                } else {
+                    paren -= 1;
+                }
+
+                if ((input[i - 1] === '{' && input[i + 1] === '}') || (input[i - 1] === '[' && input[i + 1] === ']')) {
+                    paren -= 1;
+                }
+            } else if (input[i] === '{' || input[i] === '}') {
+                if (curly === 0 && input[i] === '{') {
+                    curly += 1;
+                } else {
+                    curly -= 1;
+                }
+
+                if ((input[i - 1] === '[' && input[i + 1] === ']') || (input[i - 1] === '(' && input[i + 1] === ')')) {
+                    curly -= 1;
+                }
+            }
+        }
+    }
+
+    if (!isEven(string.length)) {
+        return false;
+    }
+    
+    return (isEven(curly) && isEven(paren) && isEven(bracket));
 }
 
 module.exports = balancedParens;
