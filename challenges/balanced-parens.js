67e9c5220d278e4323af33408b44035015c586b0
Alex Egorenkov <aegorenk@gmail.com>
2018-03-27 09:29:56 -0400

    Finish balanced-parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,44 @@
  *
  */
 
-function balancedParens(input){
 
+
+function balancedParens(input) {
+  const parenStack = [];
+  const opens = '({[';
+  const closes = ')}]';
+  const buffer = input.split('');
+
+  for (let i = 0; i < buffer.length; i += 1) {
+    let match;
+    let chr = buffer[i];
+    switch (chr) {
+      case '(':
+        parenStack.push(chr);
+        break;
+      case ')':
+        match = parenStack.pop();
+        if (match !== '(') return false;
+        break;
+      case '{':
+        parenStack.push(chr);
+        break;
+      case '}':
+        match = parenStack.pop();
+        if (match !== '{') return false;
+        break;
+      case '[':
+        parenStack.push(chr);
+        break;
+      case ']':
+        match = parenStack.pop();
+        if (match !== '[') return false;
+        break;
+      default:
+    }
+  }
+  if (parenStack.length > 0) return false;
+  return true;
 }
 
 module.exports = balancedParens;
