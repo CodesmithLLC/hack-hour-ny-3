e7ae57fabe07f1380e9786ca7de9421827a8e38f
Linda Harrison <lindafaithharrison@gmail.com>
2018-03-28 13:18:14 -0400

    49 and 19 now work

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -23,46 +23,53 @@
  * 
  */
 function romanNumeral(n) {
-    var romanNum = '';
-    var romanBase = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'};
-    if (n - 900  >= 0) {
-      romanNum = romanBase[1000];
-       n = n - 1000
+  var romanNum = '';
+  var romanBase = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'};
+  if (n - 900  >= 0) {
+    romanNum += romanBase[1000];
+     n = n - 1000
+  }
+  if (n - 400  >= 0) {
+    romanNum += romanBase[500];
+     n = n - 500
+  }
+  if (n - 90 >= 0) {
+    romanNum += romanBase[100];
+     n = n - 100
+  }
+  if (n - 40 >= 0) {
+    romanNum += romanBase[50];
+     n = n - 50
+     if (n < 0){
+       romanNum = romanBase[10] + romanNum
+       n += 10
+     }
+  } 
+ while (n - 9 >= 0) {
+    
+    romanNum += romanBase[10];
+     n = n - 10
+  } 
+  while (n - 4 >= 0) {
+    romanNum += romanBase[5];
+    n = n - 5
+  } 
+  console.log(n)
+  while (n > 0) {
+      n = n - 1
+      romanNum += romanBase[1]
     }
-    if (n - 400  >= 0) {
-      romanNum = romanBase[500];
-       n = n - 500
-    }
-    if (n - 90 >= 0) {
-      romanNum = romanBase[100];
-       n = n - 100
-    }
-    if (n - 40 >= 0) {
-      romanNum = romanBase[50];
-       n = n - 50
-    } 
-    if (n - 9 >= 0) {
-      romanNum = romanBase[10];
-       n = n - 10
-    } 
-    if (n - 4 >= 0) {
-      romanNum = romanBase[5];
-      n = n - 5
-    } 
-    while (n > 0) {
-        n = n - 1
-        romanNum += romanBase[1]
-      }
-    if (n === -100){
-      romanNum = romanBase[100] + romanNum
-    }
-    if (n === -10){
-      romanNum = romanBase[10] + romanNum
-    }
-    if (n === -1){
-      romanNum = romanBase[1] + romanNum
-    }
-    return romanNum
+  if (n === -100){
+    romanNum = romanBase[100] + romanNum
+  }
+  if (n === -10){
+    romanNum = romanBase[10] + romanNum
+  }
+  if (n === -1){
+    romanNum = romanNum.slice(0, -1)+ romanBase[1] + romanNum.slice(-1)
+  }
+  return romanNum
 }
+
 
 module.exports = romanNumeral;
