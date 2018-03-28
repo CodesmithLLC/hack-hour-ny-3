09e598ab9c158d551d786a7281f0acf8f316e33a
Richie Walker <RAW@Richies-MacBook-Pro.local>
2018-03-28 09:58:13 -0400

    Roman Numerals

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,20 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
-function romanNumeral(n) {
-
+function romanNumerals(n){
+ const decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+ const roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+ let result = '';
+ for (let i = 0; i <= decimal.length; i += 1){
+   while (n % decimal[i] < n){
+     result += roman[i];
+     n -= decimal[i];
+   }
+ }
+ return result;
 }
 
 module.exports = romanNumeral;
