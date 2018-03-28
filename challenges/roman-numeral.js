6003696c7025b3de2b3fc257f5ffc9415c976862
Alesi-Andreya Ladas <alesiladas@gmail.com>
2018-03-28 09:52:43 -0400

    HH roman numerals with another comment

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -22,6 +22,7 @@
  //Writing numerals that decrease from left to right represents addition of the numbers
 function romanNumeral(n) {
     const values = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9,V: 5, IV: 4, I: 1};
+    //hard coded values will automatically return, includes the few exxceptions for the subtraction rule
     let result = '';
     for (var key in values) { //for every key in values
         while (n >= values[key]) { //while n is greater than or equal to the value
