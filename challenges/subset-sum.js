7929a2a8ecd98c35f2432e52809aa33f025ddfb2
StephenGrable1 <stephengrable@gmail.com>
2018-03-30 10:02:28 -0400

    attempt to subset sum

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -9,7 +9,44 @@
  */
 
 function subsetSum(array, target) {
+    
 
+    
+    let startIndex = 0;
+    while (startIndex < array.length - 1){
+        for (let i = (startIndex + 1); i < array.length; i+=1){
+            console.log('checking: ', array[startIndex], "+", array[i], "=", target, "?")
+            if (array[startIndex] + array[i] === target) return true;
+        }
+        startIndex += 1;
+    }
+    
+//     tryCombos(2)
+//     tryCombos(4)
+//     function tryCombos(comboCount) {
+//         let combination = comboCount;
+//         let base = () => {
+//             let buildBase = 0;
+//             for (let i = 0; i < comboCount - 1; i+=1){
+//                 buildBase += array[i]
+//             }
+//             return buildBase;
+//         };
+    
+//         for (let i = (comboCount - 1); i < array.length; i+=1) {
+//             if (base() + array[i] === target) {
+//                 console.log("Found a two combo match: ", base(), array[i])
+//                 return true; 
+//             }
+//         }
+//         return false;
+//     }
 }
 
+
+subsetSum([3, 34, 4, 12, 5, 12], 32)
+// subsetSum([3, 7, 4, 2], 5)
+
+
+   
 module.exports = subsetSum;
