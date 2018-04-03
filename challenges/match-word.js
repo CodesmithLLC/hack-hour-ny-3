cec997e9c90904869fe1bca8a8b67e9245659b28
StephenGrable1 <stephengrable@gmail.com>
2018-04-03 10:01:47 -0400

    attempting match word, almost finished but ran out of time

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -11,7 +11,53 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+    let strArr = str.split('');
+    let wordArr = [];
+    let word = '';
 
-}
+    strArr.forEach((string, index) => {
+        if (!string.match(/[^A-Za-z]/g) || string !== '_'){
+            if (index === strArr.length-1) {
+                word += string.toLowerCase();
+                wordArr.push(word.trim())
+            };
+            word += string.toLowerCase();
+        } else {
+            console.log('not a letter')
+            if (word.length > 1){
+                wordArr.push(word)
+            };
+            word = '';
+        };
+    });
+
+    let wordOrder = []
+    for (let i = 0; i < wordArr.length; i+=1){
+        if (wordArr.indexOf(wordArr[i].split('').reverse().join('')) > -1) {
+            wordOrder.push(i);
+            console.log('This word exists in the array backwards', wordArr[i])
+
+        }
+    };
+
+    console.log(wordOrder, wordArr)
+
+    let isBalanced = false;
+    wordOrder.forEach((wordIndex, index) => {
+        if (wordIndex < wordArr[index + 1] || index === wordOrder.length){
+            console.log('we good')
+            isBalanced = true;
+        } else {
+            console.log('Out of order: ', wordArr[index], wordArr[index+1])
+            isBalanced = false;
+        }
+    })
+
+    console.log('is balanced??: ', isBalanced);
+};
+
+matchWord('if__IFF_ fi');
+// matchWord('__ENDDNE__');
+// matchWord('IF()()fi[]');
 
 module.exports = matchWord;
