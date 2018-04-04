bfd47a3246922ee03a349dbe2d864b6d54f3a1d4
StephenGrable1 <stephengrable@gmail.com>
2018-04-04 09:37:27 -0400

    first attempt at highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,19 @@
  */
 
 function highestProduct(array) {
+    array.sort((a,b) => {
+        return a - b;
+    }).reverse();
+    console.log(array);
 
-}
+    let accumulator = 1; 
+    for (let i = 0; i < 3; i+=1){
+        accumulator = accumulator * array[i];
+    };
 
+    return accumulator;
+};
+
+// console.log(highestProduct([14,9,5,3,10]))
 
 module.exports = highestProduct;
