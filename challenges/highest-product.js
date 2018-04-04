161708f3fa4bec18d02cfb778f2f7663cadade0d
jmangz <echou1@binghamton.edu>
2018-04-04 09:52:58 -0400

    refactored highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,22 +3,9 @@
  */
 
 function highestProduct(array) {
-  if (!Array.isArray(array) || array.length < 3) {
-    return 0;
-  }
-  let temp = array.slice();
-  temp.sort((a, b) => {
-    if (a - b > 0) {
-      return 1;
-    } else if (a - b < 0) {
-      return -1;
-    } else {
-      return 0;
-    }
-  });
-  if (temp[0] < 0 && temp[1] < 0) {
-    return temp[0] * temp[1] * temp[temp.length - 1];
-  }
+  if (!Array.isArray(array) || array.length < 3) return 0;
+  let temp = array.slice().sort((a, b) => a - b);
+  if (temp[0] < 0 && temp[1] < 0) return temp[0] * temp[1] * temp[temp.length - 1];
   return temp[temp.length - 1] * temp[temp.length - 2] * temp[temp.length - 3];
 }
 
