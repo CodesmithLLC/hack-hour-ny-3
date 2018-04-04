cd32720c7da20c4ae7c821d76ad8f40c079c97cb
Margaret Ma <eutopia@gmail.com>
2018-04-04 10:05:01 -0400

    hack hour april 4

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,16 @@
  */
 
 function highestProduct(array) {
-
+  if (array.length < 3) return 0
+  let newArr = array.sort((a,b) => a - b);
+  let leftProd = newArr[0] * newArr[1];
+  let rightProd = newArr[newArr.length - 1] * newArr[newArr.length - 2];
+  let rightInnerProd = newArr[newArr.length - 2] * newArr[newArr.length - 3];
+  if (leftProd > rightProd || leftProd > rightInnerProd) {
+    return leftProd * array[array.length - 1]
+  } else {
+    return array[array.length - 1] * array[array.length - 2] * array[array.length - 3]
+  }
 }
 
 
