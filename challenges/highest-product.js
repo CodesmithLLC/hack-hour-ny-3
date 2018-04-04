52cbe7b3e06d04b90d2f411c12b9e5899eebabb7
Alex Egorenkov <aegorenk@gmail.com>
2018-04-04 09:36:46 -0400

    Complete naive solution to highest product.

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,8 @@
  */
 
 function highestProduct(array) {
-
+  const multiplyAll = (arr) => arr.reduce((acc, curr) => acc * curr);
+  return multiplyAll(array.sort((a, b) => b > a).slice(0, 3)); //O(n log n) naive solution
 }
 
 
