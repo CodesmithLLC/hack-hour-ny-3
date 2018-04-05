6a91cbab964111edd54de4d84c2226a49a764fbf
Alesi-Andreya Ladas <alesiladas@gmail.com>
2018-04-05 10:00:53 -0400

    HH zipped linked list

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,27 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  if (l2.next === null) {
+    return l1;
+  } else if (l1.next === null) {
+    return l2;
+  }
+
+  let l1Curr = l1;
+  let l2Curr = l2;
+  let l1Next = l1.next;
+  let l2Next = l2.next;
+  while(l2.next !== null) {
+    l1Curr.next = l2Curr;
+    l1Next = l2Curr.next;
+    l2Next = l1Next.next;
+    l1Curr = l2Next.next;
+    l2Curr = l1Next.next;
+  }
+
+  return l1;
 };
+
+zip();
 
 module.exports = {Node: Node, zip: zip};
