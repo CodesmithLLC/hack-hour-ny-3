2c571ac91764abc65f68c3430d081216a5259b86
StephenGrable1 <stephengrable@gmail.com>
2018-04-05 10:01:18 -0400

    attempting zip linked list

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,32 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  if (l1.next === null){
+    l1.next = l2;
+    return l1;
+  }
+
+  l2 = zip(l1.next, l2);
+
+  // l1.next = l2;
+  // l2.next = l1Next;
+  // console.log(l2Next)
+  return l1;
+  
 };
+
+
+let link1 = new Node('link1');
+link1.next = new Node('link1-2');
+// link1.next.next = new Node('link-1-3');
+
+
+let link2 = new Node('link2');
+// link2.next = new Node('link2-2');
+// link2.next.next = new Node('link-2-3');
+
+// console.log(link1, link2);
+
+console.log(zip(link1, link2));
 
 module.exports = {Node: Node, zip: zip};
