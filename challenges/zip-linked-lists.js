ee64a26c13d9cf7a2072664984634887c9af242c
pkassar <paulkassar@gmail.com>
2018-04-05 10:01:31 -0400

    bug somewhere I need to fix

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -9,8 +9,31 @@ function Node(val) {
   this.value = val;
   this.next = null;
 }
-
 function zip(l1, l2) {
-};
+  while (l1.next && l2.next) {
+    let currentNode= l1
+    let nextzip1 = l1.next;
+    currentNode.next = l2;
+    l2.next = nextzip1;
+    l1 = l1.next;
+    current = l1
+  }
+}
 
-module.exports = {Node: Node, zip: zip};
+let list1 = new Node(4);
+list1.next = new Node(3);
+list1.next.next = new Node(13);
+list1.next.next.next = new Node(99);
+
+let list2 = new Node(45);
+list2.next = new Node(6);
+list2.next.next = new Node(17);
+list2.next.next.next = new Node(150);
+
+
+zip(list1, list2);
+
+
+
+
+module.exports = { Node: Node, zip: zip };
