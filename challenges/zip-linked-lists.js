d51c1793dc3dd2635b4160447f337ca59db9a0fa
jmangz <echou1@binghamton.edu>
2018-04-05 10:01:38 -0400

    zip linked list

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,7 +10,26 @@ function Node(val) {
   this.next = null;
 }
 
+let a = {val: 1, next: {val: 3, next: null}}
+let b = {val: 2, next: {val: 4, next: null}} // === {val: 1, next: {val: 2, next: {val: 3, next: {val4: null}}}}
 function zip(l1, l2) {
-};
+  if (!l1) {
+    return l2;
+  } else if (!l2) {
+    return l1;
+  }
+  let mergedHead = l1;
+  let mergedNext = l1.next;
+  let toMerge = l2;
+  let toMergeNext = l2.next;
 
+  while (toMergeNext) {
+    mergedHead.next = toMerge;
+    toMerge = mergedNext;
+    mergedNext = toMergeNext;
+    toMergeNext = toMerge.next;
+  }
+  return mergedHead;
+};
+console.log(zip(a, b));
 module.exports = {Node: Node, zip: zip};
