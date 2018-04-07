007a9fe3ab1e67a17bed0b64f7b0574be79bf553
StephenGrable1 <stephengrable@gmail.com>
2018-04-07 10:00:10 -0400

    first attempt at solution for stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,42 @@
  */
 
 function Stack() {
+  this.stack = [];
   // body...
 }
+
+Stack.prototype.push = function(element){
+  this.stack[this.stack.length] = (element);
+  return this.stack.length;
+};
+
+Stack.prototype.pop = function(){
+  let lastItem = this.stack[this.stack.length - 1]
+  delete this.stack[this.stack.length - 1]
+  return lastItem;
+};
+
+Stack.prototype.getMax = function(){
+  let highestNum;
+  for (let i = 0; i < this.stack.length - 1; i += 1){
+    if (!highestNum) highestNum = this.stack[i];
+    if (highestNum < this.stack[i]) highestNum = this.stack[i];
+  }
+  return highestNum;
+};
+
+
+//testing
+// let newStack = new Stack();
+
+// console.log(newStack.push(2));
+// console.log(newStack.push(5));
+// console.log(newStack.push(8));
+// console.log(newStack.push(27));
+// console.log(newStack);
+
+// console.log(newStack.getMax());
+// console.log(newStack.pop());
+
 
 module.exports = Stack;
