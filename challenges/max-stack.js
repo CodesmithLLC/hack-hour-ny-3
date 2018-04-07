c52df759c63db0b02e295283be9cd7140d7d14e1
Margaret Ma <eutopia@gmail.com>
2018-04-07 10:01:01 -0400

    work in progress for getMax

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,32 @@
  */
 
 function Stack() {
-  // body...
+  this.contents = []
+  this.length = 0
+  this.min = 0
+  this.max = 0
+}
+
+Stack.prototype.push = (a) => {
+  this.contents[this.length] = a;
+  this.length++;
+  if (a > this.max) {
+    this.max = a
+  } else if (a < this.min) {
+    this.min = a
+  }
+
+  return this.length
+}
+
+Stack.prototype.pop = () => {
+  let removed = this.contents[this.length - 1];
+  this.contents = this.contents.slice(0, this.length - 1);
+  return removed
+}
+
+Stack.prototype.getMax = () => {
+  return this.max
 }
 
 module.exports = Stack;
