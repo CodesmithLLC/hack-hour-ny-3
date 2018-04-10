ff11958d36973860eb09573fe2be6823cfbe4f76
haris799 <muhammad.sheikh93@yahoo.com>
2018-04-10 09:45:21 -0400

    Initial Files

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -15,6 +15,41 @@ function Node(value) {
 
 function reverseLinkedList(head) {
 
+    var currentTail = head;
+    var length = 1;
+
+    while (currentTail.next) {
+        currentTail = currentTail.next;
+        length++;
+    }
+
+
+    var currentHead = head;
+    var currentHeadVal = head.value;
+    for (var i = 0; i < Math.floor(length/2); i++) {
+        var currentTail = head;
+        var count = 1;
+        while(count < length - i) {
+            currentTail = currentTail.next;
+            count++;
+        }
+        currentHead.value = currentTail.value;
+        currentTail.value = currentHeadVal;
+        currentHead = currentHead.next;
+        currentHeadVal = currentHead.value;
+    }
+
+    return head;
 }
+
+
+var list = new Node(1);
+list.next = new Node(2);
+list.next.next = new Node(3);
+list.next.next.next = new Node(4);
+list.next.next.next.next = new Node(5);
+
+console.log(list);
+console.log(reverseLinkedList(list));
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
