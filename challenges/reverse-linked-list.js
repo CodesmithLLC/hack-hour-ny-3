781ff4bcd2a8478e7627c382391a363577f21168
Linda Harrison <lindafaithharrison@gmail.com>
2018-04-10 10:54:41 -0400

    hack hr

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,22 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-
-}
+    let valueArr = []
+    let currentVal = head.value
+    let current = head
+    while (current) {
+      valueArr.push(current.value)
+      current = current.next
+    }
+    let currentVal2 = head.value
+    let current2 = head 
+    let count = 1
+    console.log(valueArr)
+    while (current2 && count < 5) {
+      current2.value = valueArr[valueArr.length - count]
+      current2 = current2.next
+      count++
+    }
+  }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
