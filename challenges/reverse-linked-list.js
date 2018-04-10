29966fefb38796bc7cf7d8dd1672507e32fae701
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2018-04-10 09:48:19 -0400

    push

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,35 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
+    let node = head;
+    let last;
 
+    function rn(node, prev = null) {
+        if(node.next) rn(node.next, node);
+        else last = node;
+        node.next = prev;
+        
+    } rn(head);
+
+    console.log(node);
+
+    return last;
 }
+
+// function reverseLinkedList(head, prev = null, tail = null) {
+//     if(head.next) reverseLinkedList(head.next, head);
+//     if(!tail) tail = head;
+//     else if (!prev) return tail;
+//     else head.next = prev;
+// }
+
+const a = new Node(1);
+const b = new Node(2);
+const c = new Node(3);
+
+a.next = b;
+b.next = c;
+
+console.log(reverseLinkedList(a));
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
