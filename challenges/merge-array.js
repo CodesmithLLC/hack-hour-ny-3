ebfd21afc46e4b7677f725c92bd1d70b85575a64
DenaliDeMots <denali.demots@gmail.com>
2018-04-12 10:02:07 -0400

    Submission

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -13,8 +13,22 @@
  *
  */
 
-function mergeArrays(arr1, arr2) {
+mergeArrays([3, 4, 6, 10, 11, 15, 21], [1, 5, 8, 12, 14, 19])
 
+function mergeArrays(arr1, arr2) {
+    const newArr = new Array(arr1.length + arr2.length)
+    let i1 = 0
+    let i2 = 0
+    for (let i = 0; i < newArr.length; i++) {
+        if (i1 < arr1.length && (arr2[i2] === undefined || arr1[i1] < arr2[i2])) {
+            newArr[i] = arr1[i1]
+            i1++
+        } else if (i2 < arr2.length && (arr1[i1] === undefined || arr2[i2] < arr1[i1])) {
+            newArr[i] = arr2[i2]
+            i2++
+        }
+    }
+    return newArr
 }
 
 module.exports = mergeArrays;
