93df7ede88cf5978a8d42f7dd1a858c3253ec97d
StephenGrable1 <stephengrable@gmail.com>
2018-04-12 09:48:45 -0400

    attempt solution for merge ordered arrays

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -14,7 +14,39 @@
  */
 
 function mergeArrays(arr1, arr2) {
+    let currentIndex1 = 0;
+    let currentIndex2 = 0;
+    let newArr = [];
 
+    while (currentIndex1 < arr1.length && currentIndex2 < arr2.length) {
+        if (arr1[currentIndex1] < arr2[currentIndex2]){
+            newArr.push(arr1[currentIndex1]);
+            currentIndex1++;
+        } else {
+            newArr.push(arr2[currentIndex2]);
+            currentIndex2++;
+        };
+    };
+
+    if (currentIndex2 !== arr2.length){
+        const rest = arr2.splice(currentIndex2);
+        newArr.concat(rest);
+        return newArr.concat(rest);
+    };
+
+    if (currentIndex1 !== arr1.length){
+        const rest = arr1.splice(currentIndex1);
+        newArr.concat(rest);
+        return newArr.concat(rest);
+    };
+
+    return newArr;
 }
+
+// var my_array = [3,4,6,10,11,15,21];
+// var another_array = [1,5,8,12,14,19];
+
+// console.log(mergeArrays(my_array, another_array));
+ 
 
 module.exports = mergeArrays;
