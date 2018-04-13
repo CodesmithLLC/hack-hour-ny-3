c9bff8ff3b3d233646a1a0aa2e214ba70e51c398
DenaliDeMots <denali.demots@gmail.com>
2018-04-13 10:08:40 -0400

    Submission

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -12,8 +12,80 @@
  * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
  */
 
-function numToWords(num) {
-
+const digits = {
+    0: 'Zero',
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+    10: 'Ten',
+    11: 'Eleven',
+    12: 'Twelve',
+    13: 'Thirteen',
+    14: 'Fourteen',
+    15: 'Fifteen',
+    16: 'Sixteen',
+    17: 'Seventeen',
+    18: 'Eighteen',
+    19: 'Nineteen',
+    20: 'Twenty',
+    30: 'Thirty',
+    40: 'Fourty',
+    50: 'Fifty',
+    60: 'Sixty',
+    70: 'Seventy',
+    80: 'Eighty',
+    90: 'Ninety',
+    100: 'Hundred',
+    1000: 'Thousand',
+    1000000: 'Million',
+    1000000000: 'Billion',
+    1000000000000: 'Trillion',
 }
+
+function numToWords(num, string = '') {
+    const trillion = 1000000000000
+    const billion = 1000000000
+    const million = 1000000
+
+    let count = Math.floor(num / trillion)
+    if (count) string += numToWords(count) + digits[trillion]
+    num = num % trillion
+
+    count = Math.floor(num / billion)
+    if (count) string += numToWords(count) + digits[billion]
+    num = num % billion
+
+    count = Math.floor(num / million)
+    if (count) string += numToWords(count) + digits[million]
+    num = num % million
+
+    count = Math.floor(num / 1000)
+    if (count) string += numToWords(count) + digits[1000]
+    num = num % 1000
+
+    count = Math.floor(num / 100)
+    if (count) string += numToWords(count) + digits[100]
+    num = num % 100
+
+    count = Math.floor(num / 10)
+    if (count && count >= 2) {
+        string += digits[count * 10] + numToWords(num % 10)
+        num = num % 10
+    } else if (count) {
+        string += digits[num]
+        num = 0
+    }
+
+    if (num) string += digits[num]
+
+    return string
+}
+
 
 module.exports = numToWords;
