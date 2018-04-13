fcd25a8165a222386ee8c0c83a32e77c916ee4ce
jmangz <echou1@binghamton.edu>
2018-04-13 10:58:23 -0400

    hmmm

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -13,7 +13,87 @@
  */
 
 function numToWords(num) {
+  const dict = {
+    0: 'Zero',
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+    10: 'Ten',
+    11: 'Eleven',
+    12: 'Twelve',
+    13: 'Thirteen',
+    14: 'Fourteen',
+    15: 'Fifteen',
+    16: 'Sixteen',
+    17: 'Seventeen',
+    18: 'Eighteen',
+    19: 'Nineteen',
+    20: 'Twenty',
+    30: 'Thirty',
+    40: 'Fourty',
+    50: 'Fifty',
+    60: 'Sixty',
+    70: 'Seventy',
+    80: 'Eighty',
+    90: 'Ninety',
+  }
+  if (dict[num] !== undefined) {
+    return dict[num];
+  }
+  let result = '';
+  let stringed = String(num);
+  while (stringed.length > 0) {
+    if (dict[stringed] !== undefined && stringed[0] !== '0') {
+      result += dict[stringed];
+      return result;
+    }
+    if (stringed[0] === '0') {
+      stringed = stringed.slice(1);
+      numToWords(Number(stringed));
+    }
+    // if (stringed.length === 7) {
+    //   result += numToWords(Number(stringed[0])) + 'Million';
+    //   if (stringed[1] === '0') {
+    //     stringed = stringed.slice(2);
+    //   }
+    //
+    // }
+    if (stringed.length === 6) {
+      result += numToWords(stringed.slice(0, 3)) + 'Thousand';
+      stringed = stringed.slice(3);
+    }
+    if (stringed.length === 5) {
+      result += numToWords(stringed.slice(0, 2)) + 'Thousand';
+      stringed = stringed.slice(2);
+    }
+    if (stringed.length === 4) {
+      result += dict[stringed[0]] + 'Thousand';
+    }
+    if (stringed.length === 3) {
+      result += numToWords(stringed[0]) + 'Hundred' + numToWords(Number(stringed.slice(1)));
 
+    }
+
+    if (stringed.length < 3) {
+      let first = Number(stringed.slice(-1));
+      let second = Number(stringed) - first;
+      if (first === 0) {
+        result += dict[second];
+      } else {
+        result += dict[second] + dict[first];
+      }
+      return result;
+    }
+    stringed = stringed.slice(1);
+  }
+  return result;
 }
 
+console.log(numToWords(901));
 module.exports = numToWords;
