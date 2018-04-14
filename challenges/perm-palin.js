219e5893ab5c1e05355981d9648f80808ebd43ee
jmangz <echou1@binghamton.edu>
2018-04-14 10:12:44 -0400

    nilap-mrep

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -9,8 +9,36 @@
  *
  */
 
-function permPalin(str) {
-	
+function permPalin(string) {
+  var results = [];
+
+  if (string.length === 1) {
+    results.push(string);
+    return results;
+  }
+
+  for (var i = 0; i < string.length; i++) {
+    var firstChar = string[i];
+    var charsLeft = string.substring(0, i) + string.substring(i + 1);
+    var innerPermutations = permPalin(charsLeft);
+    for (var j = 0; j < innerPermutations.length; j++) {
+      results.push(firstChar + innerPermutations[j]);
+    }
+  }
+
+  return results;
 }
+
+function checkPalin(string) {
+	let perms = permPalin(string);
+	perms = perms.slice(1);
+	return perms.includes(string) ? true : false;
+}
+
+console.log(checkPalin('abab')); // => true
+console.log(checkPalin('cbaba')); // => true
+console.log(checkPalin('cbac')); // => false
+console.log(checkPalin('a')); // => true
+
 
 module.exports = permPalin;
