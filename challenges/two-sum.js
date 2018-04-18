af93aa236759ce9141ce3836bbde565ed227aee8
Margaret Ma <eutopia@gmail.com>
2018-04-18 09:30:01 -0400

    boring approach

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,11 @@
  */
 
 function twoSum(arr, n) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = (i + 1); j < arr.length; j++) {
+      if (arr[i] + arr[j] === n) return true;
+    }
+  }
 }
 
 module.exports = twoSum;
