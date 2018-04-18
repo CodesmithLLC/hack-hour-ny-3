5985e5127e55b229f77c92eb3b9b14b05053a1a4
Margaret Ma <eutopia@gmail.com>
2018-04-18 09:33:27 -0400

    add case for bad input

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,11 +3,13 @@
  */
 
 function twoSum(arr, n) {
+  if (arr.length === 0 || !Array.isArray(arr)) return false;
   for (let i = 0; i < arr.length; i++) {
     for (let j = (i + 1); j < arr.length; j++) {
       if (arr[i] + arr[j] === n) return true;
     }
   }
+  return false
 }
 
 module.exports = twoSum;
