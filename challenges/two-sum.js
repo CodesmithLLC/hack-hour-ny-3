2d95f906253e5c2b851c662d9d310b2132abfce2
Alesi-Andreya Ladas <alesiladas@gmail.com>
2018-04-18 09:19:03 -0400

    HH two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,16 +3,17 @@
  */
 
 function twoSum(arr, n) {
+    let sum = false;
     if (arr.length === 0 || !Array.isArray(arr)) return false;
     for(let i = 0; i < arr.length; i++) {
         for(let j = i + 1; j < arr.length; j++) {
             if (i + j === n) {
-                return true;
+                sum = true;
             }
         }
     }
 
-    return false;
+    return sum;
 }
 
 module.exports = twoSum;
