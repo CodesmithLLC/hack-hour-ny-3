db0b72af3be0d79db27a76006b2e663f2f973a0a
StephenGrable1 <stephengrable@gmail.com>
2018-04-18 09:22:07 -0400

    solution to two sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -4,6 +4,18 @@
 
 function twoSum(arr, n) {
 
-}
+    for (let i = 0; i < arr.length; i += 1){
+        for (let j = 0; j < arr.length; j += 1){
+            if (i !== j) {
+                if (arr[i] + arr[j] === n) return true;
+            };   
+        };
+        return false;
+    };
+};
+
+
+
+// console.log(twoSum([1,3,2], 4));
 
 module.exports = twoSum;
