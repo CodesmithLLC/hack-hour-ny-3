c9cc24621fbb9accf26cc50bbd33b630729eaafe
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2018-04-21 09:25:48 -0400

    push

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,8 +7,10 @@
  *
  */
 
-function gcd(a, b) {
-
+function gcd(a, b, i = Math.max(a, b)) {
+  if ( a % i === 0 && b % i === 0 ) return i
+  else if ( i === 1) i
+  else return gcd(a, b, i -= 1)
 }
 
 module.exports = gcd;
