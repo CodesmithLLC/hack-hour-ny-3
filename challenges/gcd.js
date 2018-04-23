48657d819576005bb801791c4f19550bb0a1fb38
Margaret Ma <eutopia@gmail.com>
2018-04-23 18:40:27 -0400

    refactored

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,12 +8,7 @@
  */
 
 function gcd(a, b) {
-  let numArr = [...Array(Math.min(a, b)+1).keys()]
-  let gcden = numArr.reduce((acc, cur) => {
-    if (a % cur === 0 && b % cur === 0) acc.push(cur)
-    return acc
-  }, [])
-  return Math.max(...gcden)
+  return (!b) ? a : gcd(b, a % b)
 }
 
 module.exports = gcd;
