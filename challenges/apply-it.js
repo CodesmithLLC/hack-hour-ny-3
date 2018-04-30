d91fe4bb3342431db8fad10991bcfb5b950056f9
DenaliDeMots <denali.demots@gmail.com>
2018-04-30 09:42:03 -0400

    submission

--- challenges/apply-it.js
+++ challenges/apply-it.js
@@ -26,7 +26,19 @@
  */
 
 function applyIt(func, args) {
-
+    return function () {
+        let a = args[0]
+        let b = args[1]
+        let c = args[2]
+        let d = args[3]
+        let e = args[4]
+        let f = args[5]
+        let g = args[6]
+        let h = args[7]
+        let i = args[8]
+        let j = args[9]
+        return func(a, b, c, d, e, f, g, h, i, j)
+    }
 }
 
 module.exports = applyIt;
