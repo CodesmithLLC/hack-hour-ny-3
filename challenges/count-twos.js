acaf5c045187cd55f3f142744fa318900d3d26d8
haris799 <muhammad.sheikh93@yahoo.com>
2018-05-01 09:14:22 -0400

    Initial Files

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -25,11 +25,4 @@ function twos(numStr) {
     return count;
 }
 
-
-console.log(countTwos(1));  //-> 0
-console.log(countTwos(3));  //-> 1
-console.log(countTwos(13));  //-> 2
-console.log(countTwos(1000));  //-> 300
-console.log(countTwos(11420));  //-> 4483
-
 module.exports = countTwos;
