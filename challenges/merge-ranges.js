d8ad88c9cea5ad2dd349f97f4325bf530e4b8c2f
Linda Harrison <lindafaithharrison@gmail.com>
2018-05-05 10:16:44 -0400

    hack hrs

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,21 @@
 
 
 function mergeRanges(array) {
-
+    var result = []
+    for (var i = 0; i < array.length - 1; i ++) {
+        var beginning = array[i][0]
+        var end = array[i][1]
+        if (end < array[i+1][0]) {
+          result.push([beginning,end])
+        } else {
+          result.push([beginning, array[i+ 1][1]])
+        
+        }
+    }
+  return result
 }
 
+
+if the 1st num is the smallest create and array
+if the second number is smaller than the first num in any array add it to the second
 module.exports = mergeRanges;
