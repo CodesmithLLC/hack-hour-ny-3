de7fe6dfa9f63139aa519e48a6f46d4f41677279
StephenGrable1 <stephengrable@gmail.com>
2018-05-08 09:18:02 -0400

    attempt multiple 3/5

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -7,9 +7,21 @@
 
 function sumMultiples3Or5Below1000() {
   let sum = 0;
-
+  for (let i = 0; i < 1000; i += 1){
+    if (i % 3 === 0 || i % 5 === 0){
+      sum += i;
+    }
+  }
   return sum;
 }
+
+
+console.log(sumMultiples3Or5Below1000())
+
+
+
+
+
 
 
 // extension make it dynamic function that takes input x,y,z
