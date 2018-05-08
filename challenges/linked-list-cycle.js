767ac7e2319db4540679668988a14e1856d6860f
jmangz <echou1@binghamton.edu>
2018-05-08 10:32:17 -0400

    rabbit and tortoise

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -33,7 +33,15 @@ var Node = function(value) {
 }
 
 function hasCycle(head) {
-
+  if (!head || !head.next) return false;
+  let turtle = head;
+  let rabbit = head.next;
+  while (rabbit && rabbit.next) {
+    if (turtle === rabbit) return true;
+    turtle = turtle.next;
+    rabbit = rabbit.next.next;
+  }
+  return false;
 }
 
 module.exports = {Node: Node, hasCycle: hasCycle}
