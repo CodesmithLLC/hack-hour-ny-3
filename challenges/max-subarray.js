877a75112945b1a3abfc7cca0a725b489e6d891e
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2018-05-09 09:44:17 -0400

    push

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,31 @@
  */
 
 function maxSubarray(arr) {
+  let max = Number.NEGATIVE_INFINITY
+  let local = 0,
+    last = arr.length + 1,
+    slow = 0,
+    fast = 1,
+    i = 0
 
+  while (fast < last) {
+    current = arr.slice(slow, fast)
+    local += current[current.length - 1]
+
+    if (local > max) max = local
+    
+    fast++
+
+    if (fast === last) {
+      local = 0
+      slow++
+      fast = slow + 1
+    }
+
+    i++
+  }
+
+  return max
 }
 
 module.exports = maxSubarray;
