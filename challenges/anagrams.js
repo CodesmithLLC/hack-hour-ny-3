c7b09e0f1369df958b318d1d3711c5c3b56faa8d
pkassar <paulkassar@gmail.com>
2018-05-10 09:49:22 -0400

    anagrams double loop

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -11,9 +11,23 @@
   * var result = anagrams('abc');
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
-
 function anagrams(string) {
+  let STORE = [];
+  if (string.length === 1) {
+    STORE.push(string);
+    return STORE;
+  }
 
+  for (let i = 0; i < string.length; i += 1) {
+    var firstChar = string[i];
+    var charLeft = string.substring(0, i) + string.substring(i + 1);
+    var innerPermutations = anagrams(charLeft);
+    for (var j = 0; j < innerPermutations.length; j += 1) {
+      STORE.push(firstChar + innerPermutations[j]);
+    }
+  }
+  return STORE;
 }
+
 
 module.exports = anagrams;
