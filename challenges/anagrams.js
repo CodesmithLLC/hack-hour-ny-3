af879a81c489c7291a8e65e491af8c00a454b429
Alesi-Andreya Ladas <alesiladas@gmail.com>
2018-05-10 10:12:40 -0400

    HH anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,20 @@
   */
 
 function anagrams(string) {
+  if (string.length < 2) return string;
+  let result = [];
 
+  for (let i = 0; i < string.length; i++) {
+    if (string.indexOf(string[i]) !== i) continue;
+    let remainingString = string.slice(0, i) + string.slice(i + 1, string.length);
+
+    for (let subPermutation of anagrams(remainingString))
+      result.push(string[i] + subPermutation)
+
+  }
+  
+  return result;
 }
+
 
 module.exports = anagrams;
