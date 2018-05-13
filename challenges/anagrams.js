1e822162b8f6f493dbbca53f8a1e7fe792d90a94
jmangz <echou1@binghamton.edu>
2018-05-13 13:07:44 -0400

    anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,29 @@
   */
 
 function anagrams(string) {
-
+  // storage for anagrams
+  const storage = {};
+  // helper function for recursive calls
+  const helper = (str, built = '') => {
+    // base case
+    if (built.length === string.length) storage[built] = true;
+    // else
+    else {
+      // loop through str
+      for (let i = 0; i < str.length; i += 1) {
+        // create new built variable so we don't have scope issues and add a letter on loop
+        const newBuilt = built + str[i];
+        // rebuild string without letter at index i
+        const rebuiltStr = str.slice(0, i) + str.slice(i + 1);
+        // call helper again
+        helper(rebuiltStr, newBuilt);
+      }
+    }
+  };
+  // call recursive function
+  helper(string);
+  // return keys of storage
+  return Object.keys(storage);
 }
 
 module.exports = anagrams;
