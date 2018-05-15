3e29c4759ba0e492324a81397119393c4855a6e5
jmangz <echou1@binghamton.edu>
2018-05-15 09:49:23 -0400

    test

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -3,9 +3,22 @@
 // https://en.wikipedia.org/wiki/Insertion_sort
 
 
-
 function insertionSort(array) {
-
+  const result = [];
+  array.forEach((num) => {
+    if (result.length === 0) result.push(num);
+    else {
+      let i = result.length - 1; // 1
+      if (num > result[i]) result.push(num);
+      else {
+        while (result[i] > num && result[i - 1] !== undefined && result[i - 1] > num) {
+          i -= 1;
+        }
+        result.splice(i, 0, num);
+      }
+    }
+  });
+  return result;
 }
 
 module.exports = insertionSort;
