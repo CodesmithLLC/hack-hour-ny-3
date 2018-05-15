7f118cb263389eae5f7f2c75ea9d02795a068f0c
Alex Egorenkov <aegorenk@gmail.com>
2018-05-15 09:47:06 -0400

    insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,18 @@
 
 
 function insertionSort(array) {
-
+  // Outer scan loop
+  for (let i = 1; i < array.length; i += 1) {
+    const pivot = array[i];
+    // Inner insertion loop
+    let j = i;
+    while (j > 0 && array[j - 1] > pivot) {
+      array[j] = array[j - 1];
+      j -= 1;
+    }
+    array[j] = pivot;
+  }
+  return array
 }
 
 module.exports = insertionSort;
