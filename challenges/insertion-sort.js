bb70d7fbf14eb5368e76b12faa9649912465047b
DenaliDeMots <denali.demots@gmail.com>
2018-05-15 10:38:46 -0400

    Submission

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,22 @@
 
 
 function insertionSort(array) {
+    for (let i = 1, j = 1, element = false; i < array.length; !element ? i++ : j--) {
+        if (!element) {
+            if (array[i] < array[i - 1]) element = true, j = i
+        } else {
+            if (j === 0 || array[i] > array[j - 1]) {
+                moveElementFromTo.call(array, i, j)
+                element = false
+            }
+        }
+    }
+    return array
+}
 
+function moveElementFromTo(sourceIndex, targetIndex) {
+    let [removed] = this.splice(sourceIndex, 1)
+    this.splice(targetIndex, 0, removed)
 }
 
 module.exports = insertionSort;
