f2292420e52ea23577be3d3a9fd5573d3cc23dc8
Alesi-Andreya Ladas <alesiladas@gmail.com>
2018-05-18 09:33:10 -0400

    HH doubly linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,29 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  if (!this.head) this.head = new Node(val);
+  else {
+    let curr = this.head;
+    while (curr.next) {
+      curr = curr.next;
+    }
+    curr.next = new Node(val);
+    curr.next.prev = curr;
+    this.tail = curr.next;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  let curr = this.head;
+  while (curr.next && curr.val !== val) {
+    curr = curr.next;
+  }
+  if (!curr.next) return undefined;
+  curr.next.prev = curr.prev;
+  curr.prev.next = curr.next;
 };
 
 module.exports = LinkedList;
