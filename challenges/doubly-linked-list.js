9df9c280781b07fe1a5ebab333656338b12ed1a1
haris799 <muhammad.sheikh93@yahoo.com>
2018-05-18 09:47:50 -0400

    Files

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,28 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  if(!this.head) {
+    this.head = new Node(val);
+    this.tail = this.head;
+  } else {
+    this.tail.next = new Node(val);
+    this.tail.next.prev = this.tail;
+    this.tail = this.tail.next;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  var current = this.head;
+  while (current) {
+    if(current.val === val) {
+      current.prev.next = current.next;
+      current.next.prev = current.prev
+    }
+    current = current.next;
+  }
 };
 
 module.exports = LinkedList;
