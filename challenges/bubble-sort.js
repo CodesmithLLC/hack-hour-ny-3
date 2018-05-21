344af35321ac086894c0eae66e7a75411e0d8a50
Alex Egorenkov <aegorenk@gmail.com>
2018-05-21 09:39:42 -0400

    bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,21 @@
 
 
 function bubbleSort(array) {
-
+  let sorted = false;
+  let rightStop = array.length;
+  while (!sorted) {
+    sorted = true;
+    for (let i = 1; i < rightStop; i += 1) {
+      if (array[i - 1] > array[i]) {
+        sorted = false;
+        //swap
+        const temp = array[i - 1];
+        array[i - 1] = array[i];
+        array[i] = temp;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
