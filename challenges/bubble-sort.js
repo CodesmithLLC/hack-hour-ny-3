0bc5f05576e26e7157051e91fe6a4737f38d3c3f
pkassar <paulkassar@gmail.com>
2018-05-21 09:51:17 -0400

    bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,24 @@
 
 
 function bubbleSort(array) {
-
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = 0, k = 1; j < array.length - i, k < array.length; j += 1, k += 1) {
+      if (array[j] > array[k]) {
+        let STORE = array[j];
+        array[j] = array[k];
+        array[k] = STORE;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
+
+// iterate over the array given to us 
+//   iterate on the array once more 
+//     compare element i and j: if i> j switch them
+//        iterate over the array once more with n-k
+// return array
+
+
