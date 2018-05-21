32aba9aa00c07104e44f53bf947e3d7e1945595b
Alesi-Andreya Ladas <alesiladas@gmail.com>
2018-05-21 09:18:55 -0400

    HH bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,22 @@
 
 
 function bubbleSort(array) {
+    let len = array.length;
 
+    while (len > 1) {
+        for (let i = 0; i < len - 1; i += 1) {
+            let ele1 = array[i];
+            let ele2 = array[i + 1];
+
+            if (ele1 > ele2) {
+                array[i] = ele2;
+                array[i + 1] = ele1;
+            }
+        }
+        len -= 1;
+    }
+
+    return array;
 }
 
 module.exports = bubbleSort;
