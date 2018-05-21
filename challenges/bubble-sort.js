a7d8fa165229d2ccfcafa41d6550dc255e93080d
jmangz <echou1@binghamton.edu>
2018-05-21 09:34:24 -0400

    test bubbler

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,20 @@
 
 
 function bubbleSort(array) {
-
+  let l = array.length;
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = 0; j < l; j += 1) {
+      const num = array[j];
+      const next = array[j + 1];
+      if (num > next) {
+        array[j + 1] = num;
+        array[j] = next;
+      }
+    }
+    l -= 1;
+  }
+  return array;
 }
+
 
 module.exports = bubbleSort;
