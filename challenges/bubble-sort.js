03e0806b9ea2749617cfcbde4f1ca81ef83e5765
DenaliDeMots <denali.demots@gmail.com>
2018-05-21 09:38:09 -0400

    Submission

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,19 @@
 
 
 function bubbleSort(array) {
-
+    if (!array || array.length < 2) return array
+    let sorted
+    while (!sorted) {
+        sorted = true
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] < array[i - 1]) {
+                let swap = array[i - 1]
+                array[i - 1] = array[i]
+                array[i] = swap
+                sorted = false
+            }
+        }
+    }
+    return array
 }
-
 module.exports = bubbleSort;
