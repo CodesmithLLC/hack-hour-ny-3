8965076d508a32de270cde70713b6f82765dbe49
Margaret Ma <eutopia@gmail.com>
2018-05-21 09:31:27 -0400

    bubbles

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,16 @@
 
 
 function bubbleSort(array) {
-
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 1; j < array.length; j++) {
+      if (array[j-1] > array[j]) {
+        let temp = array[j-1];
+        array[j-1] = array[j];
+        array[j] = temp
+      }
+    }
+  }
+  return array
 }
 
 module.exports = bubbleSort;
