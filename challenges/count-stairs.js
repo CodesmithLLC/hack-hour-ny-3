a9d05ecc5f80bcf3e94ee1019d364c420d482934
Margaret Ma <eutopia@gmail.com>
2018-05-24 09:37:22 -0400

    count stairs

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -15,7 +15,16 @@
  */
 
 function countStairs(n) {
-
+  function fib(n) {
+    if (n == 0) {
+      return n
+    } else if (n < 3) {
+      return 1
+    } else {
+      return fib(n-1) + fib(n-2)
+    }
+  }
+  return fib(n+1)
 }
 
 module.exports = countStairs;
