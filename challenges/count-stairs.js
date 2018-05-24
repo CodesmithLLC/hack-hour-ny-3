9360132e7f8e653c4ee7e6c451c2adca8ca6dd7c
Alesi-Andreya Ladas <alesiladas@gmail.com>
2018-05-24 11:23:42 -0400

    HH 5/24

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -15,13 +15,11 @@
  */
 
 function countStairs(n) {
-    let array = [1, 2]
-    if (n < 3) return array[n - 1];
-    for (let i = 2; i <= n; i++) {
-        arr.push(arr[i - 1] + arr[i - 2]);
-    }
+    if (n === 0) return 1;
+    if (n === 1) return 1;
+    if (n === 2) return 2;
 
-    return arr[n - 1];
+    return countStairs(n - 2) + countStairs(n - 1);
 }
 
 module.exports = countStairs;
