d7394c4fac6e2d32f65715d306fbd136a2bb8e1f
jmangz <echou1@binghamton.edu>
2018-05-26 17:41:56 -0400

    check bin to dec

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -3,10 +3,10 @@
  * Write a function that converts the binary string to a decimal number
  *
  * Example:
- *  binToDec('0')   -> 0
- *  binToDec('11')  -> 3
- *  binToDec('100') -> 4
- *  binToDec('101') -> 5
+* 	binToDec('0')   -> 0
+ * 	binToDec('11')  -> 3
+ * 	binToDec('100') -> 4
+ * 	binToDec('101') -> 5
  *  binToDec('0101') -> 5
  *
  * Extension:
