6812523e6f5f367203da2174b18f6da5014244c9
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2018-05-26 09:46:19 -0400

    push

--- challenges/knight-jumps.js
+++ challenges/knight-jumps.js
@@ -8,10 +8,34 @@
 // For example: if str is "(4 5)" then your program should output 8 because the knight can move to 8 different spaces
 // from position x = 4 and y = 5.
 //  example input:
-// var str = "(4 5)"
+// var str = "(4 5)" 
 
 function knightjumps(str) {
+  const checks = [[1,1], [-1,1], [1,-1],[-1,-1]]
+  let numberOfMoves = 0
 
+  for (let check of checks) {
+    let x = 1
+    let y = 3
+
+    x = x * check[0]
+    y = y * check[1]
+    
+    m = x
+    n = y
+    
+    x = parseInt(str[1]) + x
+    y = parseInt(str[3]) + y
+
+    if ( (x > 0 && y > 0) && (x < 9 && y < 9)) numberOfMoves += 1
+  
+    x = parseInt(str[1]) + n
+    y = parseInt(str[3]) + m
+
+    if ( (x > 0 && y > 0) && (x < 9 && y < 9)) numberOfMoves += 1
+  }
+
+  return numberOfMoves
 }
 
 module.exports = knightjumps;
