28c1a23852171dee5982554362d9acd74d7f49cf
jmangz <echou1@binghamton.edu>
2018-05-27 16:51:18 -0400

    refactor

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -4,21 +4,16 @@
 
 
 function insertionSort(array) {
-  const result = [];
-  array.forEach((num) => {
-    if (result.length === 0) result.push(num);
-    else {
-      let i = result.length - 1; // 1
-      if (num > result[i]) result.push(num);
-      else {
-        while (result[i] > num && result[i - 1] !== undefined && result[i - 1] > num) {
-          i -= 1;
-        }
-        result.splice(i, 0, num);
-      }
+  for (let i = 1; i < array.length; i += 1) {
+    let j = i;
+    while (array[j] < array[j - 1] && j > 0) {
+      const temp = array[j];
+      array[j] = array[j - 1];
+      j -= 1;
+      array[j] = temp;
     }
-  });
-  return result;
+  }
+  return array;
 }
 
 module.exports = insertionSort;
