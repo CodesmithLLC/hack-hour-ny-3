a11fcf07014480ce19ad1ec77912a03dc0c1bc49
jmangz <echou1@binghamton.edu>
2018-05-27 17:02:23 -0400

    refactor

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,34 +8,15 @@
  */
 
 function maxSubarray(arr) {
-  let max = null;
-  function maxArray(array) {
-    const currentMax = array.slice(1).reduce((acc, curr) => {
-      acc += curr
-      return acc;
-    }, array[0]);
-    if (max === null || max <= currentMax) max = currentMax;
-    if (array.length !== 0) maxArray(array.slice(1));
-  }
-  function maxArray2(array) {
-    const currentMax = array.slice(1).reduce((acc, curr) => {
-      acc += curr
-      return acc;
-    }, array[0]);
-    if (max === null || max <= currentMax) max = currentMax;
-    if (array.length !== 0) maxArray(array.slice(0, array.length - 1));
-  }
-  let copyBack = arr.slice();
-  let copyFront = arr.slice();
-  while (copyBack.length !== 0) {
-    maxArray(copyBack);
-    copyBack = copyBack.slice(0, copyBack.length - 1);
-  }
-  while (copyFront.length !== 0) {
-    maxArray2(copyFront);
-    copyFront = copyFront.slice(1);
-  }
-  return max;
+  let currentMax = Number.NEGATIVE_INFINITY;
+  let finalMax = Number.NEGATIVE_INFINITY;
+
+  arr.forEach((num) => {
+    currentMax = Math.max(num, currentMax + num);
+    finalMax = Math.max(finalMax, currentMax);
+  });
+
+  return finalMax;
 }
 
 module.exports = maxSubarray;
