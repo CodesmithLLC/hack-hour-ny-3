b53d37709ba7564fc1abf627abcd6151073f5e0f
jmangz <echou1@binghamton.edu>
2018-05-28 00:25:51 -0400

    MERGED

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -13,9 +13,10 @@
 function mergeRanges(array) {
   const sortArr = array.sort((a, b) => a[0] - b[0]);
   return sortArr.reduce((acc, curr) => {
-    let lastRange = acc[acc.length - 1];
-    if (lastRange[1] >= curr[0] && lastRange[1] <= curr[1]) lastRange = [lastRange[0], curr[1]];
-    else if (lastRange[1] < curr[0]) acc.push(curr);
+    const lastRange = acc[acc.length - 1];
+    if (lastRange[1] >= curr[0] && lastRange[1] <= curr[1]) {
+      acc[acc.length - 1] = [lastRange[0], curr[1]];
+    } else if (lastRange[1] < curr[0]) acc.push(curr);
     return acc;
   }, [sortArr[0]]);
 }
