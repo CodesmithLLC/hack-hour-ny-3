a2b821f2436ff67895b3b3d2649218868a86a3c8
jmangz <echou1@binghamton.edu>
2018-05-28 00:37:20 -0400

    refactor mode mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,26 +11,25 @@
 
 
 function modemean(array) {
-  let avg = Math.floor(array.reduce((acc, curr) => acc += curr, 0) / array.length);
-  let mode = array.reduce(function(acc, el) {
-    if (acc[el] === undefined) {
-      acc[el] = 1;
-    } else {
-      acc[el]++;
-    }
+  const avg = Math.floor(array.reduce((acc, curr) => acc += curr, 0) / array.length);
+  const mode = array.reduce((acc, el) => {
+    if (acc[el] === undefined) acc[el] = 1;
+    else acc[el] += 1;
     return acc;
   }, {});
+
   let highest = null;
   let highestFreq = null;
-  for (let key in mode) {
+  const modeArr = Object.keys(mode);
+
+  modeArr.forEach((key) => {
     if (highestFreq === null || mode[key] >= highestFreq) {
       highestFreq = mode[key];
       highest = Number(key);
     }
-  }
-  console.log(highest, avg);
+  });
+
   return highest === avg;
 }
-console.log(modemean([1,2,3,4,5,5]));
 
 module.exports = modemean;
