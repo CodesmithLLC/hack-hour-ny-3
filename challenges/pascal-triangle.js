2f9c1cc49e788f418f31fb7ab306e268d49ac961
Linda Harrison <lindafaithharrison@gmail.com>
2018-05-29 14:18:49 -0400

    hack hr pascal

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -33,7 +33,33 @@
 */
 
 function pascalTriangle(numRows) {
-
+  if (numRows === 0) return []
+  const result = [
+    [1]
+  ]
+  while (result.length !== numRows) {
+    let newRow = [1];
+    let index = 0;
+    let lastArr = result[result.length - 1]
+    while (newRow.length !== lastArr.length) {
+      newRow.push(lastArr[index] + lastArr[++index])
+    }
+    newRow.push(1)
+    result.push(newRow)
+  }
+  return result
 }
+
+console.log(pascalTriangle(3))
+
+// [
+//   [1],
+//   [1,1],
+//   [1,2,1]
+// ]
+
+// if (1)= 1
+// if(2) =  1,1,
+// else 1, currentArr[current] + current[next], 1 repeat
 
 module.exports = pascalTriangle;
